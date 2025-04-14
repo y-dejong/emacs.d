ae2736b451ed499ed4d2b2dc499882e7b1546966
Yasper De Jong <yasperdejong@gmail.com>
2025-04-14 16:26:11 -0700

    Minor changes

--- init.el
+++ init.el
@@ -187,30 +187,7 @@ to move before/after whitespace."
 (global-set-key (kbd "C-s") 'save-buffer)
 (global-set-key (kbd "C-r") 'revert-buffer)
 
-(ysd-require 'which-key)
-(which-key-setup-side-window-right)
-(setq which-key-idle-delay 0.4
-	  which-key-use-C-h-commands t)
 
-(defun ysd-which-key-show-top-level-excluding-ryo ()
-  (interactive)
-  (let ((ryo-modal-mode nil))
-	(which-key-show-top-level)))
-(ryo-modal-key "?" 'ysd-which-key-show-top-level-excluding-ryo)
-
-; Workaround for a paging key that has another keybind attached in underlying modes
-(defun ysd-which-key-maybe-C-h-dispatch ()
-  (interactive)
-  (if (which-key--popup-showing-p) (which-key-C-h-dispatch)
-	(let ((which-key-mode nil))
-	  (command-execute (key-binding "?")))))
-(push 'ysd-which-key-maybe-C-h-dispatch which-key--paging-functions)
-(keymap-set which-key-mode-map "?" 'ysd-which-key-maybe-C-h-dispatch)
-
-; Paging keys that match the rest of the config
-(keymap-set which-key-C-h-map "k" 'which-key-show-next-page-cycle)
-(keymap-set which-key-C-h-map "i" 'which-key-show-previous-page-cycle)
-(which-key-mode 1)
 
 (ysd-require 'counsel)
 (ivy-mode 1)
@@ -461,6 +438,7 @@ to move before/after whitespace."
 
 (global-set-key (kbd "C-e") 'treemacs)
 
+(require 'dired)
 (add-hook 'dired-mode-hook #'dired-hide-details-mode)
 (keymap-set dired-mode-map "i" 'dired-previous-line)
 (keymap-set dired-mode-map "k" 'dired-next-line)
@@ -667,7 +645,7 @@ to move before/after whitespace."
   :key groq-api-key ;; Assumes variable has been set by env.el
   :models '(llama-3.3-70b-versatile
 			llama-3.3-8b-instant
-			qwen-2.5-coder-32b
+			(qwen-2.5-coder-32b :capabilities (tool))
 			deepseek-r1-distill-llama-70b
 			qwen-qwq-32b))
 
